fa5faead7c52c1b1c93cbd040fed3d653d5b5438
Annemarie Sheridan <atsheridan38@gmail.com>
2023-01-11 19:42:55 -0800

    added comments and date fixed

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,8 @@
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
-    return date;
+    
+    return new Date (date).toDateString();
   },
   format_amount: (amount) => {
     // format large numbers with commas
